2955dceac9bdcd884f4553d5ecdbf1c132690917
administrator <administrator@BSNODDWS3F052A.BIRLASOFT.COM>
2018-09-26 19:32:26 +0530

    First Commit

---
+++ com/src/main/java/MyProject/com/App.java
@@ -0,0 +1,13 @@
+package MyProject.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
